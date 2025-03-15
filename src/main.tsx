1acbd4f8f03ac8ae18dd773b74cbadc4cd5cd47f
praveen <praveen.dusari@gmail.com>
2025-03-15 20:32:52 +0530

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@
 import { createRoot } from 'react-dom/client'
 import { StyledEngineProvider } from '@mui/material/styles';
 import './index.css'
-import App from './App.tsx'
+import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 createRoot(document.getElementById('root')!).render(
     <StyledEngineProvider injectFirst>
-    <App />
+       <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+       </BrowserRouter>
+    
     </StyledEngineProvider>
 ,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    open: true,
+    // Configure server to return index.html for unknown routes
+    hmr: true,
+  },
 })
